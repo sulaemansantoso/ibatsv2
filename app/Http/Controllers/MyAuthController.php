c766f7c8478169e56ae3e2c575ad928ee4b348d8
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-06-10 19:20:59 +0700

    adding register and login API with token support

---
+++ app/Http/Controllers/MyAuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Illuminate\Http\JsonResponse;
+
+class MyAuthController extends BaseController
+{
+   public function register(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required',
+        'kode_user' => 'required',
+    ]);
+
+    if($validator->fails()){
+        return $this->sendError('validation error', $validator->errors());
+    }
+    $input = $request->all();
+    $input['password'] = bcrypt($input['password']);
+    $user = User::create($input);
+    $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+    $success['name'] =  $user->name;
+    return $this->sendResponse($success, 'User Register Succesfully');
+
+   }
+
+   public function login (Request $request) : JsonResponse {
+    if (Auth::attempt(['kode_user' => $request->kode_user, 'password' => $request->password])){
+        $user = Auth::user();
+        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+        $success['name'] =  $user->name;
+        return $this->sendResponse($success, 'User Login Succesfully');
+    }
+    else {
+        return $this->sendError('Unauthorised', ['error'=>'Unauthorised']);
+    }
+   }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(MyAuthController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::get('/smile', function() {
+    return 'catch you smiling huh';
 });
