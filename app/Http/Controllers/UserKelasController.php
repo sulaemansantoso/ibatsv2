b3d13bdadb281c47e603c4bef8d5f41c5a71c2c1
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-10-09 18:57:09 +0700

    add get_kelas_by_id_custom with praktikum or teori

--- app/Http/Controllers/UserKelasController.php
+++ app/Http/Controllers/UserKelasController.php
@@ -37,18 +37,21 @@ class UserKelasController extends BaseController
     public function get_kelas_by_id(Request $request) {
         $kode_user = $request->kode_user;
 	$user = User::where('kode_user', $kode_user)->first();
+    $result =  UserKelas::where('id_user', $user->id)->get(['id_user_kelas','id_user', 'id_kelas']);
 
+    foreach ($result as $r) {
+        $r->kelas = $r->kelas;
+        $tipe_kelas = ($r->kelas->tipe_kelas == 0)? " Teori" : " Praktikum ";
+        $r->kelas->tipe_kelas =  $tipe_kelas;
 
-        $result =  UserKelas::where('id_user', $user->id)->get(['id_user_kelas','id_user', 'id_kelas']);
+        $r->kelas->mk = $r->kelas->mk->get();
 
-        foreach ($result as $r) {
-            $r->kelas = $r->kelas;
-            $r->kelas->mk = $r->kelas->mk->get(['kode_mk','nama_mk']);
-            // $r->user = $r->user;
-        }
-        // return $result;
-        return response()->json([
-            "data" => $result ]);
+        // $r->nama_kelas_total = $r->kelas->mk->nama_mk . " " + $tipe_kelas;
+        // $r->user = $r->user;
+    }
+    // return $result;
+    return response()->json([
+        "data" => $result ]);
     }
 
     public function get_kelas_by_id_custom (Request $request) {
@@ -66,10 +69,12 @@ class UserKelasController extends BaseController
             $temp->id_user = $r->user->kode_user;
             $temp->id_kelas = $r->kelas->id_kelas;
             $temp->kode_mk = $r->kelas->mk->kode_mk;
-            $temp->nama_mk = $r->kelas->mk->nama_mk;
+            $tipe = ($r->kelas->tipe_kelas == 0)? " Teori" : " Praktikum ";
+            $temp->nama_mk = $r->kelas->mk->nama_mk . $tipe;
             $temp->nama_kelas = $r->kelas->nama_kelas;
             $temp->jam_mulai = $r->kelas->jam_mulai;
             $temp->jam_selesai = $r->kelas->jam_selesai;
+
             $adjusted_result[] = $temp;
             // $r->user = $r->user;
         }
