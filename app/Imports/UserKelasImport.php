2584466442166e59bed0c9c9fdebef122cee3d30
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-10-10 21:33:21 +0700

    add can insert user on import data user kelas

--- app/Imports/UserKelasImport.php
+++ app/Imports/UserKelasImport.php
@@ -20,9 +20,31 @@ class UserKelasImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
-        $id_user = User::where('kode_user', $row['nrp'])->first()->id;
+        $user = User::where('kode_user', $row['nrp'])->first();
+
+        if (!is_null($user)){
+            $id_user = $user->id;
+        }
+        else {
+            $user = new User([
+                'kode_user' => $row['nrp'],
+                'name' => $row['nama'],
+                'password' => bcrypt($row['nrp']),
+                'role' => 3,
+                'email' => $row['nrp']. '@maranatha.ac.id',
+            ]);
+            $user->save();
+            $id_user = $user->id;
+        }
         $id_mk = MK::where('kode_mk', $row['kode_mk'])->first()->id_mk;
+        if (is_null($id_mk)) {
+            return;
+        }
+
         $id_kelas = Kelas::where('id_mk', $id_mk)->where('nama_kelas', $row['kelas'])->first()->id_kelas;
+        if (is_null($id_kelas)) {
+            return;
+        }
 
         $user_kelas_finder = UserKelas::where('id_user', $id_user)->where('id_kelas', $id_kelas)->first();
         if ($user_kelas_finder) {
